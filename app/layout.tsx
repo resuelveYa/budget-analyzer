512ff41cc760316c6f545e27e098553dfe6f9379
Felipe Salazar <felipeslzar@gmail.com>
2026-01-22 12:55:08 -0300

    actions updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider localization={esES} domain="resuelveya.cl">
+    <ClerkProvider localization={esES}>
       <html lang="es">
         <body className={inter.className}>{children}</body>
       </html>
